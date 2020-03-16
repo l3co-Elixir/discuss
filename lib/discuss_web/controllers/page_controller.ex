23bda10a1c5c525eb47db7cc8019761b4ed19fbb
leco <lcosta.sp.br@gmail.com>
2020-03-16 10:54:07 -0300

    feature: create my first endpoint

--- lib/discuss_web/controllers/page_controller.ex
+++ lib/discuss_web/controllers/page_controller.ex
@@ -5,6 +5,7 @@ defmodule DiscussWeb.PageController do
     render(conn, "index.html")
   end
 
+  @spec leco(Plug.Conn.t(), any) :: Plug.Conn.t()
   def leco(conn, _params) do
     json(conn, %{name: "leandro", alias: "leco"})
   end
